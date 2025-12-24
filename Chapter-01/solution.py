1e0aaf2eb8b3a5899d841c7df35d8e9c7652be17
Sahil Wadhwani <techtheera27@gmail.com>
2025-12-24 20:16:28 +0530

    Solution

--- Chapter-01/solution.py
+++ Chapter-01/solution.py
@@ -1,2 +1,13 @@
-# Chapter-01 solution
-print('Solved Chapter-01')
+'''
+1.Write a program to print Twinkle twinkle little star poem in python.
+2. Write a python program to print the contents of a directory using the os module.
+'''
+import os
+print ('''Twinkle Twinkle Little star,
+how i wonder what you are, 
+up above the world so high, 
+like a diamond in the sky\n''')
+
+
+a = "Android"
+print(os.listdir(a))
