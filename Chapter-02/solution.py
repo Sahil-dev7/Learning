467660fc62e9a34ab21203096d9a31b4d37a0fe6
Sahil Wadhwani <techtheera27@gmail.com>
2025-12-24 20:17:39 +0530

    Implement Chapter-02 solutions in solution.py
    Added solutions for Chapter-02 exercises including addition, remainder, type checking, comparison, average, and square calculations.

--- Chapter-02/solution.py
+++ Chapter-02/solution.py
@@ -1,2 +1,30 @@
-# Chapter-02 solution
-print('Solved Chapter-02')
+'''
+chapter 2
+1. Write a python program to add two numbers
+2. Write a python program to find remainder when a number is divided by z.
+3. Check the type of variable assigned using input () function.
+
+4. Use comparison operator to find out whether ‘a’ given variable a is greater than 
+
+‘b’ or not. Take a = 34 and b = 80
+
+5. Write a python program to find an average of two numbers entered by the user.
+
+6. Write a python program to calculate the square of a number entered by the user.
+'''
+
+num1 = 4
+num2 = 7
+print ("  " , num1+num2)
+
+print ("  ", num1%num2)
+
+name = input("enter your name: "); print(type(name))
+
+a = int(input("enter any number"))
+b = int(input("enter any number"))
+print ("a is greater than b is ",a>b)
+
+print ("average of those 2 numbers is ",(a+b)/2)
+
+print ("square of given number is ",a*a)
